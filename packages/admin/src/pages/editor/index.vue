f131aa2ba4462e09d785b905c0bfad88dfd7156f
lwdggm <lwdggm@gmail.com>
2017-08-24 02:19:21 +0800

    up

--- packages/admin/src/pages/editor/index.vue
+++ packages/admin/src/pages/editor/index.vue
@@ -61,6 +61,7 @@ export default {
         .fetch()
         .then(({ path, content, sha }) => {
           // path = path.replace(/^_posts\//, '').split('/');
+          path = path.split('/')
           this.title = path.pop();
           this.path = path.join('/') || '';
           this.sha = sha;
@@ -83,6 +84,7 @@ export default {
         .then(({ path, content }) => {
           this.loading = false;
           // path = path.replace(/^_posts\//, '').split('/');
+          path = path.split('/')
           this.title = path.pop();
           this.path = path.join('/') || '';
           this.rawContent = 'data:image/png;base64,' + content;
